13df5aaa3b32bb58c4c78205f69476d0d2fd1d36
Paweł Prorok <pawel3474@gmail.com>
2022-06-28 17:52:41 +0200

    Add function that create new notes

--- script.js
+++ script.js
@@ -21,16 +21,47 @@ const closePanel = () => {
 	notePanel.style.display = 'none';
 	error.style.visibility = 'hidden';
 	textArea.value = '';
-    category.selectedIndex = 0;
+	category.selectedIndex = 0;
 };
 
 const addNote = () => {
-	if (textArea.value !== ''  && category.options[category.selectedIndex].value !== '0') {
+	if (
+		textArea.value !== '' &&
+		category.options[category.selectedIndex].value !== '0'
+	) {
+		createNote();
 		error.style.visibility = 'hidden';
 	} else {
 		error.style.visibility = 'visible';
 	}
 };
+
+const createNote = () => {
+	const newNote = document.createElement('div');
+	newNote.classList.add('note');
+	newNote.setAttribute('id', cardID);
+	newNote.innerHTML = `<div class="note-header">
+                            <h3 class="note-title">${selectedValue}</h3>
+                            <button class="delete-note">
+                            <i class="fas fa-times icon"></i>
+                            </button>
+                        </div>
+                        <div class="note-body">
+                            ${textArea.value}
+                        </div>`;
+
+	noteArea.appendChild(newNote);
+	cardID++;
+    textArea.value = '';
+	category.selectedIndex = 0;
+    notePanel.style.display = 'none';
+
+};
+
+const selectValue = () => {
+	selectedValue = category.options[category.selectedIndex].text;
+};
+
 
 addBtn.addEventListener('click', openPanel);
 cancelBtn.addEventListener('click', closePanel);
